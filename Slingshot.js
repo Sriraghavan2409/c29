9365f2dbb7a550705c2cd5c73c38f9caa124c998
Sriraghavan2409 <68691479+Sriraghavan2409@users.noreply.github.com>
2020-08-31 16:54:42 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,59 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        //Load all the slingshot images from the sprites folder
+        this.sling1=loadImage("sprites/sling1.png");
+        this.sling2=loadImage("sprites/sling2.png");
+        this.sling3=loadImage("sprites/sling3.png");
+
+
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+    display(){
+        //Display the slingShot
+        image(this.sling1,200,20)
+        image(this.sling2,170,20)
+        
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            push();
+            //Set the colour for the rubberband to be drawn getting the values from colorzilla
+            stroke(48,22,8);
+            //To prevent the base of the rubberBand from getting displayed behind the bird when bird is in the front of slingShot 
+          if(pointA.x<220){
+              //Rubberband appears thinner when streched 
+            strokeWeight(7);
+            //Draw the rubberBand using lines
+            line(pointA.x-20,pointA.y,pointB.x-10,pointB.y);
+            line(pointA.x-20,pointA.y,pointB.x+30,pointB.y-3);
+            //display the base of the rubberband
+            image(this.sling3,pointA.x-30,pointA.y-10,15,30)
+
+          }
+          else{
+              strokeWeight(3);
+            line(pointA.x+25,pointA.y,pointB.x-10,pointB.y);
+            line(pointA.x+25,pointA.y,pointB.x+30,pointB.y-3);
+            image(this.sling3,pointA.x+25,pointA.y-10,15,30)
+
+          }
+           pop(); 
+         
+            
+        }
+    }
+    
+}
